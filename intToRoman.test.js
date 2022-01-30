a27ac3c1e917728a8594e190e541729c4df82098
SukirtiSaxena <sukirtisaxena@gmail.com>
2022-01-30 16:52:08 +0000

    2nd test case for int 2 - failed

--- intToRoman.test.js
+++ intToRoman.test.js
@@ -4,4 +4,8 @@ describe("Number to Roman Numeral",() => {
     test("Number 1 is equivalent to --> I ", () => {
         expect(roman(1)).toBe('I');
     });
+    test("Number 2 is equivalent to --> II ", () => {
+        expect(roman(2)).toBe('II');
+    });
+
 });
