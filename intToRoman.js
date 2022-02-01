5be725fba17870e1b87e0875d79998b67be70873
SukirtiSaxena <sukirtisaxena@gmail.com>
2022-02-01 16:25:40 +0000

    refactored code

--- intToRoman.js
+++ intToRoman.js
@@ -1,11 +1,12 @@
 
 const roman = num => {
-    if (num === 1)
-        return 'I';
-    if (num === 2)
-        return 'II';
-    if (num === 3)
-        return 'III';
+    let result = '';
+
+    while (num > 0) {
+        result += 'I';
+        num--;
+    };
+    return result;
 };
 
 module.exports = { roman };
